5f376dea7002a56b05e6a409e50c7212bda0087f
ryeballar@gmail.com <ryeballar@gmail.com>
2014-02-12 00:20:32 +0800

    ..

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
+
+print_r($_SERVER);
+exit;
+
 if (isset($_SERVER['PLATFORM']))
 {
     switch ($_SERVER['PLATFORM']) {
