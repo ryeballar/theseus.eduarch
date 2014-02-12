000ef8f4899ff6e1d69d04434dbc3fdd78bfd5c3
ryeballar@gmail.com <ryeballar@gmail.com>
2014-02-12 18:56:40 +0800

    fb

--- application/controllers/signup.php
+++ application/controllers/signup.php
@@ -1,4 +1,6 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once 'sdk/facebook-php/src/facebook.php';
     	
 class signup extends Base_Controller {
 
@@ -7,6 +9,12 @@ class signup extends Base_Controller {
 	}
 
 	function index() {
+		$config = array(
+			'appId' => '599535443458840',
+			'secret' => '61e6578c042ccce513f7016a3aea4362',
+			'fileUpload' => false,
+			'allowSignedRequest' => false
+		);
 		set_active('SignUp');
 		$this->load('signup', 'Sign Up');
 	}
